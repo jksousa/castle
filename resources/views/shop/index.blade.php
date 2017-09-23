5c9fad1e08f9f3366d7f912192dea59c92bf1083
jeff sousa <jeff@jksousa.com>
2017-09-22 21:00:55 -0400

    making progress

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -5,5 +5,106 @@
 @endsection
 
 @section('content')
-    <h1>it works</h1>
+    <div class="container-fluid">
+        <div class="row">
+            <nav class="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
+                <ul class="nav nav-pills flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Overview <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Reports</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Analytics</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Export</a>
+                    </li>
+                </ul>
+
+                <ul class="nav nav-pills flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Nav item</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Nav item again</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">One more nav</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Another nav item</a>
+                    </li>
+                </ul>
+
+                <ul class="nav nav-pills flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Nav item again</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">One more nav</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Another nav item</a>
+                    </li>
+                </ul>
+            </nav>
+
+            <main class="col-sm-9 ml-sm-auto col-md-10 pt-3" role="main">
+                <h1>Dashboard</h1>
+
+                <section class="row text-center placeholders">
+                    <div class="col-6 col-sm-3 placeholder">
+                        <img src="data:image/gif;base64,R0lGODlhAQABAIABAAJ12AAAACwAAAAAAQABAAACAkQBADs=" width="200" height="200" class="img-fluid rounded-circle" alt="Generic placeholder thumbnail">
+                        <h4>Label</h4>
+                        <div class="text-muted">Something else</div>
+                    </div>
+                    <div class="col-6 col-sm-3 placeholder">
+                        <img src="data:image/gif;base64,R0lGODlhAQABAIABAADcgwAAACwAAAAAAQABAAACAkQBADs=" width="200" height="200" class="img-fluid rounded-circle" alt="Generic placeholder thumbnail">
+                        <h4>Label</h4>
+                        <span class="text-muted">Something else</span>
+                    </div>
+                    <div class="col-6 col-sm-3 placeholder">
+                        <img src="data:image/gif;base64,R0lGODlhAQABAIABAAJ12AAAACwAAAAAAQABAAACAkQBADs=" width="200" height="200" class="img-fluid rounded-circle" alt="Generic placeholder thumbnail">
+                        <h4>Label</h4>
+                        <span class="text-muted">Something else</span>
+                    </div>
+                    <div class="col-6 col-sm-3 placeholder">
+                        <img src="data:image/gif;base64,R0lGODlhAQABAIABAADcgwAAACwAAAAAAQABAAACAkQBADs=" width="200" height="200" class="img-fluid rounded-circle" alt="Generic placeholder thumbnail">
+                        <h4>Label</h4>
+                        <span class="text-muted">Something else</span>
+                    </div>
+                </section>
+
+                <h2>Test Cases</h2>
+                <div class="table-responsive">
+                    <table class="table table-striped">
+                        <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Location</th>
+                            <th>Engineer</th>
+                            <th>Speed</th>
+                            <th>Errors</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($testCaseData as $testCaseRow)
+                        <tr>
+                            <td>{{$testCaseRow->id}}</td>
+
+                            <td>{{$testCaseRow->location}}</td>
+                            <td>{{$testCaseRow->engineer}}</td>
+                            <td>{{$testCaseRow->speed}}</td>
+                            <td>{{$testCaseRow->errors}}</td>
+                        </tr>
+                        @endforeach
+
+                        </tbody>
+                    </table>
+                </div>
+            </main>
+        </div>
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,48 @@
 |
 */
 
-Route::get('/', function () {
-    return view('shop.index');
+Route::get('/', [
+    'uses'=>'TestCaseController@getTestCases',
+    'as'=>'testcase.index'
+]);
+
+Route::group(['prefix' => 'user'], function () {
+
+    Route::group(['middleware'=>'guest'], function () {
+
+        Route::get('/signup', [
+            'uses'=>'UserController@getSignup',
+            'as'=>'user.signup'
+        ]);
+
+        Route::post('/signup',[
+            'uses' => 'UserController@postSignup',
+            'as' => 'user.signup'
+        ]);
+
+
+        Route::get('/signin', [
+            'uses'=>'UserController@getSignin',
+            'as'=>'user.signin'
+        ]);
+
+        Route::post('/signin',[
+            'uses' => 'UserController@postSignin',
+            'as' => 'user.signuin'
+        ]);
+    });
+
+    Route::group(['middleware'=>'auth'], function () {
+
+
+        Route::get('/profile', [
+            'uses' => 'UserController@getProfile',
+            'as' => 'user.profile'
+        ]);
+
+        Route::get('/logout', [
+            'uses' => 'UserController@getLogout',
+            'as' => 'user.logout'
+        ]);
+    });
 });
