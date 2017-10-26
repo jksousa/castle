7f5496683a74997258b2239fc494c13d386d88a7
vagrant <vagrant@homestead>
2017-10-26 17:07:32 +0000

    update home page with js

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,25 +5,31 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title')</title>
-    <link rel="stylesheet" href="{{URL::to("css/dashboard.css")}}" >
+    <title>JKSousa</title>
+    <link rel="stylesheet" href="{{URL::to("css/dashboard.css")}}">
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css"
+          integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{URL::to("css/cube.css")}}">
+
     @yield('styles')
     <style>
         body {
             background: #555555;
         }
+
         .perfect-centering {
             position: absolute;
             text-align: center;
             top: 50%;
-            left:50%;
-            transform: translate(-50%,-50%);
+            left: 50%;
+            transform: translate(-50%, -50%);
         }
+
         .rounded {
             border-radius: 100rem;
         }
+
 
     </style>
 </head>
@@ -31,17 +37,39 @@
 
 <div class="container perfect-centering ">
 
-        <img class="rounded" src="images/jksousa.png" alt="">
+    {{--<img class="rounded img-fluid" src="images/jksousa.png" alt="" >--}}
 
-</div>
+    <div class="wrap">
+        <div class="row">
+            <div class="cube">
+                <div class="face"></div>
+                <div class="face">
+                    <div class="perfect-centering">
+                       <button class="btn btn-secondary btn-sm">jeff@jksousa.com</button>
+                    </div>
+                </div>
+                <div class="face"></div>
+                <div class="face"></div>
+                <div class="face"></div>
+                <div class="face"></div>
+            </div>
+
+        </div>
 
 
+    </div>
 
 
-<script src="https://use.fontawesome.com/704bb61c39.js"></script>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+    <script src="https://use.fontawesome.com/704bb61c39.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+            integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+            crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"
+            integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
+            crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"
+            integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1"
+            crossorigin="anonymous"></script>
 @yield('scripts')
 
 </body>
